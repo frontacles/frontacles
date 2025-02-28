19ec0da7fe89950d50b9103340c842bd87ab07d5
Mehdi M. <hi@mehdi.cc>
2025-02-28 16:58:49 +0100

    Please ESLint Please ESLint even more

--- src/url/url.test.js
+++ src/url/url.test.js
@@ -1,9 +1,9 @@
 import { describe, expect, test } from 'vitest'
 import { Email } from '..'
 
-describe.only('url/email', () => {
+describe('url/email', () => {
   test.todo('Email is a class')
-  // test.todo('Email is a class', () => expect(round).toBeInstanceOf(Function))
+  // sometest.todo('Email is a class', () => expect(round).toBeInstanceOf(Function))
 
   const myEmail = new Email('someone@domain.tld')
 
@@ -16,7 +16,7 @@ describe.only('url/email', () => {
   test('cast to a string with String', () => expect((new String(myEmail)).toString()).toBe('someone@domain.tld'))
   test('cast to a string with `litteral`', () => expect(`${myEmail}`).toBe('someone@domain.tld'))
 
-  // test('username and domain are no-writable', () => {
+  // sometest('username and domain are no-writable', () => {
   //   expect(myEmail.username = 'someone-else').toThrow()
   //   expect(myEmail.host = 'other-domain.tld').toThrow()
   // })
