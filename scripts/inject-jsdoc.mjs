7ad7b1cdfc7ae58074d9e72020ca30e976e620d4
Mehdi M. <hi@mehdi.cc>
2025-03-01 15:49:07 +0100

    Update JSDoc injection script

--- scripts/inject-jsdoc.mjs
+++ scripts/inject-jsdoc.mjs
@@ -1,7 +1,9 @@
 /**
  * @file Inject JSDoc comment in the generated types. This script currently
  * limits itself to override the description of class members inheriting
- * their implementation from the parent class but a wrong description.
+ * implementation from the parent class, but with a wrong description.
+ *
+ * This is a JSDoc limitation, not a TypeScript one.
  *
  * @todo Make a system to move the new documentation to an external file.
  */
@@ -12,19 +14,19 @@ readFile('types/index.d.ts', 'utf-8', function (err, data) {
 
   const emailClassNeedle = 'export class Email extends URL {'
 
-  data = data.replace(emailClassNeedle, emailClassNeedle
-    + '\n\t/**'
-    + '\n\t * The domain name and extension of the email.'
-    + '\n\t *'
-    + '\n\t * In `username@domain.tld`, it is `domain.tld`.'
-    + '\n\t */'
-    + '\n\tdeclare hostname: string;'
-    + '\n\t/**'
-    + '\n\t * The username of the email.'
-    + '\n\t *'
-    + '\n\t * In `username@domain.tld`, it is `username`.'
-    + '\n\t */'
-    + '\n\tdeclare username: string;'
+  data = data.replace(emailClassNeedle, `${emailClassNeedle}
+	/**
+	 * The domain name and extension of the email.
+	 *
+	 * In \`username@domain.tld\`, it is \`domain.tld\`.
+	 */
+	declare hostname: string;
+	/**
+	 * The username of the email.
+	 *
+	 * In \`username@domain.tld\`, it is \`username\`.
+	 */
+	declare username: string;`
   )
 
   writeFile('types/index.d.ts', data, () => {});
