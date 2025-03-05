a8a493a5234b0bc67f3617b260a56449029cbc43
Mehdi <9340937+meduzen@users.noreply.github.com>
2025-03-05 10:29:39 +0100

    Add `clamp` function

--- src/math/index.js
+++ src/math/index.js
@@ -1,4 +1,22 @@
 /**
+ * Make sure a number stays between boundaries.
+ * Follows {@link https://github.com/tc39/proposal-math-clamp TC39 proposal}.
+ *
+ * Examples:
+ *
+ * ```
+ * clamp(17, 3, 8)  // 8
+ * clamp(-3, 3, 8)  // 3
+ * clamp(5, 3, 8)   // 5
+ * ```
+ *
+ * @param {number} val The number you want to maintain inside boundaries.
+ * @param {number} min The lowest permitted value.
+ * @param {number} max The highest permitted value.
+ */
+export const clamp = (val, min, max) => Math.max(min, Math.min(max, val))
+
+/**
  * Round a number to the (optionally) provided precision.
  *
  * Examples:
