1acaa0e645f874ac3fa07a3ab976e874e9b730f9
Mehdi M. <hi@mehdi.cc>
2025-03-07 01:37:12 +0100

    Remove unnecessary character escape

--- src/url/email.js
+++ src/url/email.js
@@ -58,4 +58,4 @@ export class Email extends URL {
  *
  * @param {string|any} address
  */
-export const isEmail = address => /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(address)
+export const isEmail = address => /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(address)
