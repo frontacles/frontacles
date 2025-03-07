2638dfe3c0d35673ec50656828ecf79728a1ae3f
Mehdi M. <hi@mehdi.cc>
2025-03-07 01:31:01 +0100

    Iterate over `Email` benchmarking

--- src/url/bench/email.bench.js
+++ src/url/bench/email.bench.js
@@ -1,6 +1,6 @@
 import { bench, describe } from 'vitest'
 
-import { Email } from '../..'
+import { EmailClassWithURL, EmailClassWithRegExp, isEmailWithRegExp } from './email.implementations'
 
 import joi from 'joi'
 import isEmail from 'validator/es/lib/isEmail'
@@ -9,22 +9,25 @@ import yup from 'yup'
 import { z } from 'zod'
 
 describe('url/email passing all tests', () => {
-  const EmailClass = Email
+  const EmailWithURL = EmailClassWithURL
+  const EmailWithRegExp = EmailClassWithRegExp
+  const emailWithRegExp = isEmailWithRegExp
   const validatorIsEmail = isEmail
   const y = yup
 
   const yupSchema = y.string().email()
 
-  bench('`Email.canParse()`', () => EmailClass.canParse('someone@domain.tld'))
+  bench('`EmailWithURL.canParse` (current)', () => EmailWithURL.canParse('someone@domain.tld'))
+  bench('`EmailClassWithRegExp.canParse`', () => EmailWithRegExp.canParse('someone@domain.tld'))
+  bench('`emailWithRegExp`', () => emailWithRegExp('someone@domain.tld'))
 
   bench('`validator/isEmail`', () => validatorIsEmail('someone@domain.tld'))
 
-  bench('`yup create schema and .isValidSync()`', () => y.string().email().isValidSync('someone@domain.tld'))
-  bench('`yup precalculated schema .isValidSync()`', () => yupSchema.isValidSync('someone@domain.tld'))
+  bench('yup create schema and `isValidSync`', () => y.string().email().isValidSync('someone@domain.tld'))
+  bench('yup precalculated schema `isValidSync`', () => yupSchema.isValidSync('someone@domain.tld'))
 })
 
-describe('url/email failing all tests', () => {
-  const EmailClass = Email
+describe('url/email not passing all tests', () => {
   const j = joi
   const v = valibot
   const zod = z
@@ -33,17 +36,15 @@ describe('url/email failing all tests', () => {
   const vSchema = v.pipe(v.string(), v.email())
   const zodSchema = zod.string().email()
 
-  bench('`Email.canParse()`', () => EmailClass.canParse('someone@domain.tld'))
+  bench('joi create schema and `.validate`', () => j.string().email().validate('someone@domain.tld'))
+  bench('joi precalculated schema `.validate`', () => joiSchema.validate('someone@domain.tld'))
 
-  bench('`joi create schema and .validate()`', () => j.string().email().validate('someone@domain.tld'))
-  bench('`joi precalculated schema .validate()`', () => joiSchema.validate('someone@domain.tld'))
-
-  bench('`valibot create schema and .is()`', () => {
+  bench('valibot create schema and `.is`', () => {
     const vEmailSchema = v.pipe(v.string(), v.email())
     v.is(vEmailSchema, 'someone@domain.tld')
   })
-  bench('`valibot precalculated schema .is()`', () => v.is(vSchema, 'someone@domain.tld'))
+  bench('valibot precalculated schema `.is`', () => v.is(vSchema, 'someone@domain.tld'))
 
-  bench('`zod create schema and .parse()`', () => zod.string().email().parse('someone@domain.tld'))
-  bench('`zod precalculated schema .parse()`', () => zodSchema.parse('someone@domain.tld'))
+  bench('zod create schema and `.parse`', () => zod.string().email().parse('someone@domain.tld'))
+  bench('zod precalculated schema `.parse`', () => zodSchema.parse('someone@domain.tld'))
 })
---
+++ src/url/bench/email.implementations.js
@@ -0,0 +1,68 @@
+/** @file Alternatives implementations of email validation for benchmark purpose. */
+
+/** A function validating an email, using RegExp. */
+export const isEmailWithRegExp = address => /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(address)
+
+/** Class extending `URL`. Validation using RegExp. */
+// eslint-disable-next-line no-undef
+export class EmailClassWithRegExp extends URL {
+
+  constructor(address) {
+    super(`ftp://${address}`)
+
+    if (!this.#validate(address)) {
+      throw new TypeError(`Email constructor: ${address} is not a valid Email.`)
+    }
+  }
+
+  toJSON () {
+    return this.toString()
+  }
+
+  toString () {
+    return `${this.username}@${this.hostname}`
+  }
+
+  /** @param {string} address */
+  #validate (address) {
+    return this.toString() == address && this.username
+  }
+
+  /** @param {any|Email} address */
+  static canParse(address) {
+    return /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(address)
+  }
+}
+
+/** Class extending `URL`. Validation using `URL`. */
+// eslint-disable-next-line no-undef
+export class EmailClassWithURL extends URL {
+  constructor(address) {
+    super(`ftp://${address}`)
+
+    if (!this.#validate(address)) {
+      throw new TypeError(`Email constructor: ${address} is not a valid Email.`)
+    }
+  }
+
+  toJSON () {
+    return this.toString()
+  }
+
+  toString () {
+    return `${this.username}@${this.hostname}`
+  }
+
+  #validate (address) {
+    return this.toString() == address && this.username
+  }
+
+  static canParse(address) {
+    try {
+      new this(address)
+      return true
+    } catch {
+      return false
+    }
+  }
+}
