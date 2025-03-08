a2c7b13fb1242718e41268aad399ba341559ce65
Mehdi M. <hi@mehdi.cc>
2025-03-08 11:45:12 +0100

    Update `Email.toString` documentation to reduce user noise.

--- src/url/email.js
+++ src/url/email.js
@@ -17,6 +17,9 @@ export class Email extends URL {
   }
 
   /**
+   * The email address (`username@domain.tld`) as a string.
+   *
+   * (maintainer comment)
    * Replace the string representation of the top-level class (`URL`) to be an
    * email address instead of `ftp://username@domain.tld`. It is needed for
    * situation where type casting to string is involved (`console.log`…).
